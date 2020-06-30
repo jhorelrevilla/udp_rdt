a1ce6768b95475eac083cb713059a6e6cc4f137f
jhorelrevilla <jhorel.revilla@ucsp.edu.pe>
2020-06-29 20:52:55 -0500

    update

--- client.cpp
+++ client.cpp
@@ -9,7 +9,7 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <errno.h>
-#include <ctime>
+#include <vector>
 using namespace std;
 
 #define PORT	 8080 
@@ -24,34 +24,52 @@ bool wait(double t){
 	}
 	return true;
 }
+vector<string> paquetes(string t,int prot){
+	vector<string> temp;
+	while (t.size()>prot){
+		string tem=t.substr(0,prot);
+		temp.push_back(tem);
+		t=t.substr(prot,t.size());
+	}
+	temp.push_back(t);
+	return temp;
+}
+bool verificar(string msg,int mod,int check){
+	int suma=0;
+	for (int i = 0; i < msg.size(); i++){
+		suma+=int(msg[i]);
+	}
+	suma=suma%mod;
+	if(suma==check)
+		return true;
+	return false;
+}
 
-int main() { 
-	int sockfd; 
+int enviar(string msg){
+	int sockfd;
 	char buffer[MAXLINE]; 
-	string msg = "msg from client"; 
-	struct sockaddr_in	 servaddr; 
-
+	struct sockaddr_in	 servaddr;
+	vector<string> package;
 	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { perror("socket creation failed"); exit(EXIT_FAILURE); } 
 	memset(&servaddr, 0, sizeof(servaddr)); 
 	servaddr.sin_family = AF_INET; 
 	servaddr.sin_port = htons(PORT); 
 	servaddr.sin_addr.s_addr = INADDR_ANY; 
-	
-	int n, len; 
-	while (true){
-		cout<<"Escribe q para salir\nEnviar mensaje al server: ";	
-		getline(cin,msg);
-		if(msg=="q")
-			break;
-		sendto(sockfd, msg.c_str(), msg.size()  , MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr)); 
-		cout<<"Mensaje enviado"<<endl;
-			
-		n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &servaddr,(socklen_t *) &len); 
+	if (msg.size()>500){
+		for(int i=0;i<msg.size();i+=500){
 
-		buffer[n] = '\0'; 
-		cout<<"El server envio "<<buffer<<endl;
-		cout<<"//////////////////////////////"<<endl;
+		}
 	}
+	///////Formar mensaje
+	sendto(sockfd, msg.c_str(), msg.size()  , MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr)); 
+	///////
+	cout<<"Mensaje enviado"<<endl;
 	close(sockfd); 
+}
+
+
+int main() { 
+	string test="prueba1";
+	enviar(test);
 	return 0; 
 } 
